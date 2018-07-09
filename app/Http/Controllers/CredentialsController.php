605d9f65e61c4e44bb716c3d54491fe1306dc811
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2018-07-09 15:13:38 +0200

    fill out other methods

--- app/Http/Controllers/CredentialsController.php
+++ app/Http/Controllers/CredentialsController.php
@@ -33,6 +33,7 @@ class CredentialsController extends Controller
 
         $newCreds = new Credentials();
         $newCreds->_id = Uuid::generate(4)->string;
+        $newCreds->name = $request->get('name');
         $newCreds->username = $request->get('username');
         $newCreds->secret = Crypt::encrypt($request->get('secret'));
         $newCreds->domain = $request->get('domain');
@@ -48,24 +49,60 @@ class CredentialsController extends Controller
         return response()->json(['credentials_id' => $newCreds->_id], 201);
     }
 
-    public function getCredentials()
+    public function getAllCredentials()
     {
-
+        $credentials = Auth::user()->credentials()->all();
+        return response()->json(['credentials' => $credentials], 200);
     }
 
-    public function editCredentials(Request $request)
+    public function getCredentials($credentialId)
+    {
+        $credential = Credentials::loadFromUuid($credentialId);
+
+        if (Auth::user()->id !== $credential->user->id) {
+            return response()->json(['error' => 'Invalid credential ID']);
+        }
+
+        return response()->json(['credentials' => $credential], 200);
+    }
+
+    public function editCredentials(Request $request, $credentialId)
     {
         try {
             $this->validate($request, [
-
+                'name' => 'required|string',
+                'username' => 'required|string',
+                'secret' => 'required|string',
+                'domain' => 'required|string',
+                'mail_from_address' => 'required|string',
+                'mail_from_name' => 'required|string',
             ]);
-        } catch (\Exception $e) {
-
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
         }
+        $credential = Credentials::loadFromUuid($credentialId);
+        $credential->name = $request->get('name');
+        $credential->username = $request->get('username');
+        $credential->secret = Crypt::encrypt($request->get('secret'));
+        $credential->domain = $request->get('domain');
+        $credential->mail_from_address = $request->get('mail_from_address');
+        $credential->mail_from_name = $request->get('mail_from_name');
+        $credential->updated_by = Auth::user()->id;
+        $credential->save();
+
+        return response()->json(['success' => true], 202);
     }
 
     public function deleteCredentials($credentialId)
     {
+        $credential = Credentials::loadFromUuid($credentialId);
 
+        if (Auth::user()->id !== $credential->user->id) {
+            return response()->json(['error' => 'Invalid credential ID']);
+        }
+        $credential->deleted_by = Auth::user();
+        $credential->delete();
+
+        return response()->json(['success' => true], 202);
     }
 }

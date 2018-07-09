6450cc57841ef622b37271c9a107d778fbdc58d4
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2018-07-09 14:39:21 +0200

    add method stubs to controller

--- app/Credentials.php
+++ app/Credentials.php
@@ -10,4 +10,18 @@ class Credentials extends BaseModel
     use SoftDeletes;
 
     public $timestamps = true;
+
+    protected $hidden = [
+        'id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function provider()
+    {
+        return $this->belongsTo(Provider::class);
+    }
 }
--- app/Http/Controllers/CredentialController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-
-class CredentialController extends Controller
-{
-
-}

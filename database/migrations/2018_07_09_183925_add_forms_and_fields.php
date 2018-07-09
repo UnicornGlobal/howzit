d00100e1cdb644034b653b516e55b53f57c2441e
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2018-07-09 18:47:08 +0200

    add migrations for forms and fields

---
+++ database/migrations/2018_07_09_183925_add_forms_and_fields.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFormsAndFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (env('APP_ENV') !== 'production') {
+            Schema::dropIfExists('forms');
+            Schema::dropIfExists('fields');
+        }
+
+        Schema::create('forms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('_id')->unique();
+            $table->string('name');
+            $table->text('response_template');
+            $table->unsignedBigInteger('credentials_id');
+            $table->timestamps();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->unsignedBigInteger('deleted_by');
+            $table->softDeletes();
+        });
+
+        Schema::create('fields', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('_id')->unique();
+            $table->string('name');
+            $table->unsignedInteger('max_length');
+            $table->unsignedBigInteger('form_id');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->unsignedBigInteger('deleted_by');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+        Schema::dropIfExists('fields');
+    }
+}

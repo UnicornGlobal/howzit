821633f23874b6ef8e8ba3d59211651fa8cf9ca8
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2018-07-10 09:22:53 +0200

    Creating form test passing

--- app/Form.php
+++ app/Form.php
@@ -3,6 +3,7 @@
 namespace App;
 
 
+use Fields;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Form extends BaseModel
@@ -10,4 +11,20 @@ class Form extends BaseModel
     use SoftDeletes;
 
     public $timestamps = true;
+
+    protected $hidden = [
+        'id',
+        'deleted_at',
+        'credentials_id',
+    ];
+
+    public function fields()
+    {
+        return $this->hasMany(Fields::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id', 'created_by');
+    }
 }

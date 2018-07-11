84e83eb78b98e339465d9f0c1d22180a304ccb3a
Fergus Strangways-Dixon <fergusdixon101@gmail.com>
2018-07-11 15:43:18 +0200

    added migrations and models for responses

---
+++ database/seeds/Responses.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Response;
+use App\ResponseElement;
+use Illuminate\Database\Seeder;
+
+class Responses extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Response::firstOrCreate([
+            'id' => 1,
+            '_id' =>'128010d0-a816-4322-bc23-2e431232cd5b',
+            'form_id' => 1,
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        ResponseElement::firstOrCreate([
+            'id' => 1,
+            'field_id' => 1,
+            'answer' => 'testuser@yahoo.com',
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        ResponseElement::firstOrCreate([
+            'id' => 1,
+            'field_id' => 2,
+            'answer' => 'Tom Smith',
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        ResponseElement::firstOrCreate([
+            'id' => 1,
+            'field_id' => 3,
+            'answer' => 'tabbs',
+            'created_by' => 1,
+            'updated_by' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}

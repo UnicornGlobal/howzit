a15025a5b7af4d49c144137a02bc21b52b63c9d6
Darryn Ten <darryn.ten@gmail.com>
2018-08-29 14:11:36 +0200

    Adding app id

--- config/cors.php
+++ config/cors.php
@@ -11,7 +11,7 @@ return [
     'supportsCredentials' => true,
     'allowedOrigins' => ['*'],
     'allowedHeaders' => ['Content-Type', 'Content-Length', 'Origin', 'X-Requested-With', 'Debug-Token', 'Registration-Access-Key', 'X-CSRF-Token', 'App', 'User-Agent', 'Authorization'],
-    'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE', 'OPTIONS'],
+    'allowedMethods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     'exposedHeaders' => ['Authorization'],
     'maxAge' => 0,
 ];
